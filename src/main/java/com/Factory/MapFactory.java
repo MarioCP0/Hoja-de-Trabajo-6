8d570ebfe0cb99b58c9197f3204197d53db3c241
PeDro0210 <avi23089@uvg.edu.gt>
2024-03-06 08:46:29 -0600

    initial commit

---
+++ src/main/java/com/Factory/MapFactory.java
@@ -0,0 +1,23 @@
+package com.Factory;
+import java.util.Map;
+
+import java.util.HashMap;
+import java.util.TreeMap;
+import java.util.LinkedHashMap;
+
+public class MapFactory<T,K> {
+
+    public Map<T,K> CreateMap(MapEnum mapEnum){
+        switch(mapEnum){
+            case HASHMAP:
+                return new HashMap<T,K>();
+            case TREEMAP:
+                return new TreeMap<T,K>();
+            case LINKEDHASHMAP:
+                return new LinkedHashMap<T,K>();
+            default:
+                return null;
+        }
+    }
+
+}
